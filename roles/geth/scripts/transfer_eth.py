1e084376b94c18e916fdb93108a5f56f5e1002da
lighthouse-ansible <lighthouse-ansible@sigmaprime.io>
2020-04-16 02:00:19 +0000

    Progress with ETH provisioning

--- roles/geth/scripts/transfer_eth.py
+++ roles/geth/scripts/transfer_eth.py
@@ -12,8 +12,13 @@ eth_amount = sys.argv[4]
 web3 = Web3(IPCProvider(ipc_endpoint))
 web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-web3.eth.sendTransaction({
+tx = web3.eth.sendTransaction({
     'from': from_addr,
     'to': to_addr,
     'value': web3.toWei(eth_amount, 'ether')
 })
+
+hex_str = ''.join('{:02x}'.format(x) for x in tx)
+
+sys.stdout.write(hex_str)
+sys.stdout.flush()
